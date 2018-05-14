ae1458e690b41f4c64e37d3290bb01023614b753
MichaelRinglein <michaelringlein@gmail.com>
2018-05-14 23:37:20 +0300

    Classification Example with California census

---
+++ Classification_California_Census-data.py
@@ -0,0 +1,88 @@
+# Goal: predict the income of given census data
+
+# The Data
+
+import pandas as pd 
+census = pd.read_csv("census_data.csv") 
+
+## Tensorflow doesn't understand <50k and >50k -> Convert to 0 and 1
+def label_fix(label):
+    if label == ' <=50k':
+        return 0
+    else:
+        return 1
+
+census['income_bracket'] = census['income_bracket'].apply(label_fix)
+
+
+# Train Test Split 
+
+from sklearn.model_selection import train_test_split
+x_data = census.drop('income_bracket', axis=1)
+y_labels = census['income_bracket']
+X_train, X_test, y_train, y_test = train_test_split(x_data, y_labels, test_size=0.3)
+
+
+# Feature Columns
+
+import tensorflow as tf 
+
+## Categorial Values
+gender = tf.feature_column.categorical_column_with_vocabulary_list("gender", ["Female", "Male"])
+occupation = tf.feature_column.categorical_column_with_hash_bucket("occupation", hash_bucket_size=1000)
+marital_status = tf.feature_column.categorical_column_with_hash_bucket("marital_status", hash_bucket_size=1000)
+relationship = tf.feature_column.categorical_column_with_hash_bucket("relationship", hash_bucket_size=1000)
+education = tf.feature_column.categorical_column_with_hash_bucket("education", hash_bucket_size=1000)
+workclass = tf.feature_column.categorical_column_with_hash_bucket("workclass", hash_bucket_size=1000)
+native_country = tf.feature_column.categorical_column_with_hash_bucket("native_country", hash_bucket_size=1000)
+
+## Continuous Values
+age = tf.feature_column.numeric_column("age")
+education_num = tf.feature_column.numeric_column("education_num")
+capital_gain = tf.feature_column.numeric_column("capital_gain")
+capital_loss = tf.feature_column.numeric_column("capital_loss")
+hours_per_week = tf.feature_column.numeric_column("hours_per_week")
+
+## All together
+feat_cols = [gender,occupation,marital_status,relationship,education,workclass,native_country,
+            age,education_num,capital_gain,capital_loss,hours_per_week]
+
+
+# Input Function
+input_func = tf.estimator.inputs.pandas_input_fn(
+    x=X_train,
+    y=y_train,
+    batch_size=100,
+    num_epochs=None,
+    shuffle=True
+)
+
+
+# The Model
+
+model = tf.estimator.LinearClassifier(feature_columns=feat_cols)
+
+## Training the model
+model.train(input_fn=input_func, steps=5000)
+
+
+# Evaluation
+
+pred_fn = tf.estimator.inputs.pandas_input_fn(
+    x=X_test,
+    batch_size=len(X_test),
+    shuffle=False
+)
+
+predictions = list(model.predict(input_fn=pred_fn))
+
+
+# Classification
+
+final_preds = []
+for pred in predictions:
+    final_preds.append(pred['class_ids'][0])
+
+## Handy tool for classification report
+from sklearn.metrics import classification_report
+print(classification_report(y_test, final_preds))
